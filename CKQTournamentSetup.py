d69e8ba496a407ae5b7c60859184226c2d0259fb
GOTWIC <50720527+GOTWIC@users.noreply.github.com>
2022-02-23 22:48:31 -0500

    Add files via upload

--- CKQTournamentSetup.py
+++ CKQTournamentSetup.py
@@ -1,11 +1,16 @@
 import berserk
 import os
 from dotenv import load_dotenv
+from pprint import pprint
+import datetime
 
 load_dotenv()
 
 session = berserk.TokenSession(os.getenv('lichessToken'))
 client = berserk.Client(session=session)
 
-print(client.account.get())
+
+pprint(client.tournaments.create(clock_time=10, clock_increment=5, minutes=70, name="Pawn 3 and Knight Arena", start_date='2022-02-24T23:30:00.000Z', rated=True, berserkable=False))
+
+
 
